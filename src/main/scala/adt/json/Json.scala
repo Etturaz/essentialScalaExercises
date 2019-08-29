fcfe26fd08830eea1840316f3a7f7a5766e64895
Etturaz <babbo@guampile-laptop.localdomain>
2019-08-29 17:50:56 +0200

    update

--- src/main/scala/adt/json/Json.scala
+++ src/main/scala/adt/json/Json.scala
@@ -1,32 +1,36 @@
 package adt
 package json
 
-sealed trait Json {js =>
+sealed trait Json { js =>
 
   override def toString: String = {
     val quoteStr = '"'.toString
     def quote(str: String): String =
       s"$quoteStr$str$quoteStr"
 
+    /**
+      * For the variable binding pattern ref. to Programming in Scala p.320
+      */
     def seqToJson(seq: SeqCell): String = seq match {
       case SeqCell(h, t @ SeqCell(_, _)) => s"${h.toString}, ${seqToJson(t)}"
       case SeqCell(h, SeqEnd)            => h.toString
     }
 
     def objectToJson(job: ObjectCell): String = job match {
-        case ObjectCell(k, v, t @ ObjectCell(_, _, _)) => s"${quote(k)}: ${v.toString}, ${objectToJson(t)}"
-        case ObjectCell(k, v, ObjectEnd) => s"${quote(k)}: ${v.toString}"
+      case ObjectCell(k, v, t @ ObjectCell(_, _, _)) =>
+        s"${quote(k)}: ${v.toString}, ${objectToJson(t)}"
+      case ObjectCell(k, v, ObjectEnd) => s"${quote(k)}: ${v.toString}"
     }
 
     js match {
-      case JsBoolean(value) => value.toString
-      case JsNumber(value)  => value.toString
-      case JsString(value)  => value
-      case JsNull => "null"
-      case s @ SeqCell(_, _) => s"[${seqToJson(s)}]"
+      case JsBoolean(value)        => value.toString
+      case JsNumber(value)         => value.toString
+      case JsString(value)         => value
+      case JsNull                  => "null"
+      case s @ SeqCell(_, _)       => s"[${seqToJson(s)}]"
       case o @ ObjectCell(_, _, _) => s"{${objectToJson(o)}}"
-      case SeqEnd => "[]"
-      case ObjectEnd => "{}"
+      case SeqEnd                  => "[]"
+      case ObjectEnd               => "{}"
 
     }
   }
